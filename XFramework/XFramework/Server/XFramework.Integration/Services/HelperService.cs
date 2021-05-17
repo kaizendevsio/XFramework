aeb91dc3bd81d2ab1dfc72f6918e716a77dabd33
Xeon <jjeeraldo@gmail.com>
2021-05-17 18:53:56 +0800

    Updated:  - Helper Service

--- XFramework/XFramework/Server/XFramework.Integration/Services/HelperService.cs
+++ XFramework/XFramework/Server/XFramework.Integration/Services/HelperService.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Security.Cryptography;
 using System.Text;
 using XFramework.Integration.Interfaces;
 using XFramework.Integration.Services.Helpers;
@@ -18,6 +19,14 @@ namespace XFramework.Integration.Services
             return Encoding.ASCII.GetString(b);
         }
 
+        public string GenerateReferenceString()
+        {
+            var ticks = new DateTime(2021,1,1).Ticks;
+            var ans = DateTime.Now.Ticks - ticks;
+            var uniqueId = ans.ToString("x").ToUpper();
+            return uniqueId;
+        }
+        
         public HttpHelper Http { get; set; } = new();
         public StopWatchHelper StopWatch { get; set; } = new();
     }
