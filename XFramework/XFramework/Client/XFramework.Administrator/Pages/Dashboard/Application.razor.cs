f5bd9c25ff6789d243dd93f64feae34adaca91ce
Marquesa <jem.broasgarcia@gmail.com>
2022-03-21 18:35:34 +0800

    Sample Tables

--- XFramework/XFramework/Client/XFramework.Administrator/Pages/Dashboard/Application.razor.cs
+++ XFramework/XFramework/Client/XFramework.Administrator/Pages/Dashboard/Application.razor.cs
@@ -1,20 +1,12 @@
-﻿using MudBlazor;
-using MudBlazor.Examples.Data.Models;
-
-namespace XFramework.Administrator.Pages.Dashboard;
+﻿namespace XFramework.Administrator.Pages.Dashboard;
 
 public class ApplicationBase : PageBase
 {
     public bool dense = false;
-    public bool hover = true;
-    public string searchString1 = "";
     public bool sort = true;
     public bool multiSelect = true;
     public bool pagination;
-    public bool ronly = false;
     public bool bordered = false;
     private string searchString = "";
-    
-    private List<string> editEvents = new();
     
 }
