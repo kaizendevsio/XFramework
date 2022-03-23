fec8b0c2a4281606e69d875be96a9af4cebf7281
Xeon <jjeeraldo@gmail.com>
2022-03-23 17:52:41 +0800

    - Added sample table

--- XFramework/XFramework/Client/XFramework.Administrator/Pages/PageBase.cs
+++ XFramework/XFramework/Client/XFramework.Administrator/Pages/PageBase.cs
@@ -14,7 +14,7 @@ public class PageBase : BlazorStateComponent
    [Inject] public HttpClient httpClient { get; set; }
 
    public SessionState SessionState => GetState<SessionState>();
-   public List<SampleModels> Model { get; set; } = new();
+   public List<SampleModels> Model { get; set; } = new(){new(){Id = 1},new(){Id = 2},new(){Id = 3}};
    public async Task NavigateTo(string url)
    {
       NavigationManager.NavigateTo(url);
