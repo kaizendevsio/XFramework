5a739b2fbdb957f9a5c5d21f36e691cf4377962b
Marquesa <jem.broasgarcia@gmail.com>
2022-03-31 13:36:32 +0800

    Update Modal

--- XFramework/XFramework/Client/XFramework.Administrator/Pages/Dashboard/Enterprises.razor.cs
+++ XFramework/XFramework/Client/XFramework.Administrator/Pages/Dashboard/Enterprises.razor.cs
@@ -7,8 +7,12 @@ public class EnterprisesBase : PageBase
 {
     DialogOptions maxWidth = new DialogOptions() { MaxWidth = MaxWidth.Medium, FullWidth = true };
     
-    public void OpenDialog(DialogOptions options)
+    public void CreateModal(DialogOptions options)
     {
         DialogService.Show<CreateModal>();
+    } 
+    public void UpdateModal(DialogOptions options)
+    {
+        DialogService.Show<UpdateModal>();
     }
 }
