7fb2f2bd950a63ce672905894bacdb78e9413aef
Xeon <jjeeraldo@gmail.com>
2022-04-12 21:09:02 +0800

    - Added Crypto Functionalities

--- XFramework/XFramework/Client/XFramework.Client.Shared/Core/Features/Session/Handlers/LogInHandler.cs
+++ XFramework/XFramework/Client/XFramework.Client.Shared/Core/Features/Session/Handlers/LogInHandler.cs
@@ -86,7 +86,7 @@ public partial class SessionState
                             {
                                 await Mediator.Send(new WalletState.GetWalletList());
                             });
-                        }, null, (int) action.AutoRefreshWalletsInterval.TotalMilliseconds, (int) action.AutoRefreshWalletsInterval.TotalMilliseconds);
+                        }, null, 0, (int) action.AutoRefreshWalletsInterval.TotalMilliseconds);
                     }
                 }
                 catch (Exception e)
