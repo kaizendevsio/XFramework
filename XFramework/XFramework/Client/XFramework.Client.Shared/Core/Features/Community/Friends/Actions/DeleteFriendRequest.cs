7851c223c6099901f0aecfc6f2905711e4e9bf0e
Xeon <jjeeraldo@gmail.com>
2022-04-23 16:36:50 +0800

    Updated Client

---
+++ XFramework/XFramework/Client/XFramework.Client.Shared/Core/Features/Community/Friends/Actions/DeleteFriendRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace XFramework.Client.Shared.Core.Features.Community;
+
+public partial class CommunityState
+{
+    public class DeleteFriendRequest : IAction
+    {
+        public Guid? Guid { get; set; }
+    }
+}
---
+++ XFramework/XFramework/Client/XFramework.Client.Shared/Core/Features/Community/Friends/Handlers/DeleteFriendRequestHandler.cs
@@ -0,0 +1,47 @@
+﻿using Blazored.LocalStorage;
+using Community.Integration.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace XFramework.Client.Shared.Core.Features.Community;
+
+public partial class CommunityState
+{
+    public class DeleteFriendRequestHandler: ActionHandler<DeleteFriendRequest>
+    {
+        public ICommunityServiceWrapper CommunityServiceWrapper { get; }
+        private CommunityState CurrentState => Store.GetState<CommunityState>();
+
+        public DeleteFriendRequestHandler(ICommunityServiceWrapper communityServiceWrapper, IConfiguration configuration, ISessionStorageService sessionStorageService,
+            ILocalStorageService localStorageService, SweetAlertService sweetAlertService,
+            NavigationManager navigationManager, EndPointsModel endPoints, IHttpClient httpClient,
+            HttpClient baseHttpClient, IJSRuntime jsRuntime, IMediator mediator, IStore store) : base(configuration,
+            sessionStorageService, localStorageService, sweetAlertService, navigationManager, endPoints, httpClient,
+            baseHttpClient, jsRuntime, mediator, store)
+        {
+            CommunityServiceWrapper = communityServiceWrapper;
+            Configuration = configuration;
+            SessionStorageService = sessionStorageService;
+            LocalStorageService = localStorageService;
+            SweetAlertService = sweetAlertService;
+            NavigationManager = navigationManager;
+            EndPoints = endPoints;
+            HttpClient = httpClient;
+            BaseHttpClient = baseHttpClient;
+            JsRuntime = jsRuntime;
+            Mediator = mediator;
+            Store = store;
+        }
+
+        public override async Task<Unit> Handle(DeleteFriendRequest action, CancellationToken aCancellationToken)
+        {
+            var result = await CommunityServiceWrapper.DeleteConnection(new()
+            {
+                Guid = action.Guid
+            });
+            
+            await HandleFailure(result, action);
+            await HandleSuccess(result, action);
+            return Unit.Value;
+        }
+    }
+}
