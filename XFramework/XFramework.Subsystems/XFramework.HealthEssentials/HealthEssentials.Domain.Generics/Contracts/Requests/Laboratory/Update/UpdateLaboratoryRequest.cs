00afd6b9e32b4429ed230fbbaebe97aadb13b8f2
Xeon <jjeeraldo@gmail.com>
2022-07-18 13:15:16 +0800

    Safety Backup

--- XFramework/XFramework.Subsystems/XFramework.HealthEssentials/HealthEssentials.Domain.Generics/Contracts/Requests/Laboratory/Update/UpdateLaboratoryRequest.cs
+++ XFramework/XFramework.Subsystems/XFramework.HealthEssentials/HealthEssentials.Domain.Generics/Contracts/Requests/Laboratory/Update/UpdateLaboratoryRequest.cs
@@ -4,5 +4,12 @@ namespace HealthEssentials.Domain.Generics.Contracts.Requests.Laboratory.Update;
 
 public class UpdateLaboratoryRequest : RequestBase
 {
+    public string? Name { get; set; }
+    public string? ShortName { get; set; }
+    public string? Description { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public string? Website { get; set; }
+    public string? Logo { get; set; }
     public GenericStatusType Status { get; set; }
 }
--- XFramework/XFramework.Subsystems/XFramework.HealthEssentials/HealthEssentials.Domain.Generics/Contracts/Requests/Pharmacy/Update/UpdatePharmacyLocationRequest.cs
+++ XFramework/XFramework.Subsystems/XFramework.HealthEssentials/HealthEssentials.Domain.Generics/Contracts/Requests/Pharmacy/Update/UpdatePharmacyLocationRequest.cs
@@ -4,20 +4,19 @@ namespace HealthEssentials.Domain.Generics.Contracts.Requests.Pharmacy.Update;
 
 public class UpdatePharmacyLocationRequest : RequestBase
 {
-    public long PharmacyId { get; set; }
+    public Guid? PharmacyGuid { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
     public string? UnitNumber { get; set; }
     public string? Street { get; set; }
     public string? Building { get; set; }
-    public long? BarangayGuid { get; set; }
-    public long? CityGuid { get; set; }
+    public Guid? BarangayGuid { get; set; }
+    public Guid? CityGuid { get; set; }
     public string? Subdivision { get; set; }
-    public long? RegionGuid { get; set; }
+    public Guid? RegionGuid { get; set; }
     public bool? MainAddress { get; set; }
-    public long? ProvinceGuid { get; set; }
-    public long? CountryGuid { get; set; }
-    public Guid? Guid { get; set; }
+    public Guid? ProvinceGuid { get; set; }
+    public Guid? CountryGuid { get; set; }
     public GenericStatusType Status { get; set; }
     public string? Phone { get; set; }
     public string? Email { get; set; }
