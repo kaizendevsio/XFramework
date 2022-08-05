1cc88e1879dcdcc9c3b92f6126392251480e4bb9
Xeon <eraldo.xeon@gmail.com>
2022-08-05 09:48:35 +0800

    - Added Tag CRUD

--- XFramework/XFramework.Subsystems/XFramework.HealthEssentials/HealthEssentials.Api/SignalR/Handlers/Schedule/Update/UpdateScheduleHandler.cs
+++ XFramework/XFramework.Subsystems/XFramework.HealthEssentials/HealthEssentials.Api/SignalR/Handlers/Schedule/Update/UpdateScheduleHandler.cs
@@ -1,6 +1,12 @@
-﻿namespace HealthEssentials.Api.SignalR.Handlers.Schedule.Update;
+﻿using HealthEssentials.Core.DataAccess.Commands.Entity.Schedule;
+using HealthEssentials.Domain.Generics.Contracts.Requests.Schedule.Update;
 
-public class UpdateScheduleHandler
+namespace HealthEssentials.Api.SignalR.Handlers.Schedule.Update;
+
+public class UpdateScheduleHandler : BaseSignalRHandler, ISignalREventHandler
 {
-    
+    public void Handle(HubConnection connection, IMediator mediator)
+    {
+        HandleRequestCmd<UpdateScheduleRequest, UpdateScheduleCmd>(connection, mediator);
+    }
 }
---
+++ XFramework/XFramework.Subsystems/XFramework.HealthEssentials/HealthEssentials.Core/DataAccess/Commands/Entity/Tag/DeleteTagEntityCmd.cs
@@ -0,0 +1,8 @@
+﻿using HealthEssentials.Domain.Generics.Contracts.Requests.Tag.Delete;
+
+namespace HealthEssentials.Core.DataAccess.Commands.Entity.Tag;
+
+public class DeleteTagEntityCmd : DeleteTagEntityRequest, IRequest<CmdResponse<DeleteTagEntityCmd>>
+{
+    
+}
