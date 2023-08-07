48b6c8695c4bb5288b34ed1b901cef373838084e
Xeon <jjeeraldo@gmail.com>
2023-08-08 02:10:51 +0800

    - Cleaned core project

--- XFramework/XFramework/Server/XFramework.Core/DataAccess/Query/Entity/Identity/GetIdentityQuery.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace XFramework.Core.DataAccess.Query.Entity.Identity
-{
-    public class GetIdentityQuery : IRequest<QueryResponse<IdentityResponse>>
-    {
-        public Guid Uid { get; set; }   
-    }
-}
