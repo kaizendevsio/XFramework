81f802748e301a404d8e32691be071e8b6a9f929
Xeon <jjeeraldo@gmail.com>
2024-03-14 14:16:47 +0800

    - Removed production credentials - Moved SEQ connection string to env variable

--- XFramework/XFramework.Subsystems/XFramework.HealthEssentials/HealthEssentials.Api/Installers/DbInstaller.cs
+++ XFramework/XFramework.Subsystems/XFramework.HealthEssentials/HealthEssentials.Api/Installers/DbInstaller.cs
@@ -9,7 +9,7 @@ public class DbInstaller : IInstaller
     public virtual void InstallServices(IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<DbContext, HealthEssentialsContext>(options => options
-                .UseNpgsql(configuration.GetConnectionString("HealthDatabaseConnection"))
+                .UseNpgsql(configuration.GetConnectionString(configuration["HealthDatabaseConnection"] ?? "HealthDatabaseConnection"))
                 .ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.BoolWithDefaultWarning))
             );
     }
--- XFramework/XFramework.Subsystems/XFramework.Wallets/Wallets.Api/Installers/DbInstaller.cs
+++ XFramework/XFramework.Subsystems/XFramework.Wallets/Wallets.Api/Installers/DbInstaller.cs
@@ -7,7 +7,7 @@ public class DbInstaller : IInstaller
     public virtual void InstallServices(IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<DbContext, AppDbContext>(options => options
-            .UseNpgsql(configuration.GetConnectionString("DefaultDatabaseConnection"))
+            .UseNpgsql(configuration.GetConnectionString(configuration["DefaultDatabaseConnection"] ?? "DefaultConnection"))
             .ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.BoolWithDefaultWarning))
         );
     }
