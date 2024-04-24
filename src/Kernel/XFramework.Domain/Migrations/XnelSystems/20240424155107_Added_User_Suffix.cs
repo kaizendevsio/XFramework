5c065d7c1902a9fc71d6e850d7a8c9049cbc252f
Xeon <jjeeraldo@gmail.com>
2024-04-25 00:04:34 +0800

    - Added Microsoft.EntityFrameworkCore.Design package reference to XFramework.Gateway.csproj - Generated new files for migration to add 'Suffix' column under the 'Identity' schema in 'IdentityInformation' table - Generated new auto-generated files for migration detailing the changes made in the database model.

---
+++ src/Kernel/XFramework.Domain/Migrations/XnelSystems/20240424155107_Added_User_Suffix.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XFramework.Domain.Migrations.XnelSystems
+{
+    /// <inheritdoc />
+    public partial class Added_User_Suffix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Suffix",
+                schema: "Identity",
+                table: "IdentityInformation",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Suffix",
+                schema: "Identity",
+                table: "IdentityInformation");
+        }
+    }
+}
