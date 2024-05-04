0837e2f6cac1e517455aeaad56e70eb2c66f7f14
Xeon <jjeeraldo@gmail.com>
2024-05-04 16:01:43 +0800

    Refactored the RequestBase class in Shared Contracts, extracted IRequest interfaces into ICommand and IQuery interfaces

--- src/Shared/XFramework.Domain.Shared/Contracts/Requests/RequestBase.cs
+++ src/Shared/XFramework.Domain.Shared/Contracts/Requests/RequestBase.cs
@@ -5,13 +5,17 @@ using XFramework.Domain.Shared.Contracts.Responses;
 
 namespace XFramework.Domain.Shared.Contracts.Requests;
 
-public record Create<T>(T Model) : RequestBase, IRequest<CmdResponse<T>>;
+public interface ICommand : IRequest<CmdResponse>;
+public interface ICommand<T> : IRequest<CmdResponse<T>>;
+public interface IQuery<T> : IRequest<QueryResponse<T>>;
 
-public record Patch<T>(T Model) : RequestBase, IRequest<CmdResponse<T>>;
+public record Create<T>(T Model) : RequestBase, ICommand<T>;
 
-public record Replace<T>(T Model) : RequestBase, IRequest<CmdResponse<T>>;
+public record Patch<T>(T Model) : RequestBase, ICommand<T>;
 
-public record Delete<T>(T Model) : RequestBase, IRequest<CmdResponse<T>>;
+public record Replace<T>(T Model) : RequestBase, ICommand<T>;
+
+public record Delete<T>(T Model) : RequestBase, ICommand<T>;
 
 public record GetList<T>(
     int PageSize,
@@ -21,7 +25,7 @@ public record GetList<T>(
     int NavigationDepth = 1,
     bool? IncludeNavigations = false,
     List<QueryFilter>? Filter = null,
-    List<string>? Includes = null) : RequestBase, IRequest<QueryResponse<PaginatedResult<T>>>;
+    List<string>? Includes = null) : RequestBase, IQuery<PaginatedResult<T>>;
 
 public record Get<T>(
     Guid Id, 
@@ -29,7 +33,7 @@ public record Get<T>(
     bool NoCache = true, 
     int NavigationDepth = 1,
     bool? IncludeNavigations = null,
-    List<string>? Includes = null) : RequestBase, IRequest<QueryResponse<T>>;
+    List<string>? Includes = null) : RequestBase, IQuery<T>;
 
 public record RequestBase : IHasRequestServer
 {
