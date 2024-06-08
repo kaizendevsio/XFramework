f023e539887f359c8072f7a75b22bdd04f3d5897
Xeon <jjeeraldo@gmail.com>
2024-06-08 20:37:21 +0800

    Added Receive SMS logging

---
+++ src/Modules/XFramework.SmsGateway/SmsGateway.Core/Commands/Sms/CreateMessageReceived.cs
@@ -0,0 +1,21 @@
+﻿using XFramework.Domain.Shared.Contracts.Requests;
+
+namespace SmsGateway.Core.Commands.Sms;
+
+public record CreateMessageReceived : RequestBase, ICommand
+{
+    public string? Sender { get; set; }
+    public string? Message { get; set; }
+    public string? SubscriptionId { get; set; }
+    public string? ReceivedAt { get; set; }
+    public Guid AgentClusterId { get; set; }
+}
+
+
+public class CreateMessageReceivedHandler : IRequestHandler<CreateMessageReceived, CmdResponse>
+{
+    public Task<CmdResponse> Handle(CreateMessageReceived request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
