d795c925f1539087be483cca09ce8e52e8dd3567
Xeon <jjeeraldo@gmail.com>
2024-06-24 15:47:28 +0800

    Added wallet updates event

--- src/Modules/XFramework.Wallets/Wallets.Domain.Shared/Constants.cs
+++ src/Modules/XFramework.Wallets/Wallets.Domain.Shared/Constants.cs
@@ -6,4 +6,9 @@ public static class Constants
     {
         public static readonly Guid Php = new Guid("7ee3621a-5878-4c16-8112-eab11f29db95");
     }
+
+    public static class WalletEvents
+    {
+        public static readonly string WalletUpdated = nameof(WalletUpdated);
+    }
 }
