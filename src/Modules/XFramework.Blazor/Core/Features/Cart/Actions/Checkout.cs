fa854b3a957d5683a69ca28d10acdbce0575a361
Xeon <eraldo.xeon@gmail.com>
2025-04-13 16:36:01 +0800

    feat(Cart): Enhance cart functionality and component integration
    - Added CartState property to XLayoutBase for easier access in components.
    - Updated Checkout action to include an EventCallback parameter for post-checkout logic.
    - Refactored XFramework.Blazor project file for better organization of project references.

--- src/Modules/XFramework.Blazor/Core/Features/Cart/Actions/Checkout.cs
+++ src/Modules/XFramework.Blazor/Core/Features/Cart/Actions/Checkout.cs
@@ -4,13 +4,17 @@ namespace XFramework.Blazor.Core.Features.Cart;
 
 public partial class CartState
 {
-    public record Checkout : StateAction;
+    public record Checkout(EventCallback Callback) : StateAction;
 
     public class CheckoutHandler(HandlerServices handlerServices, IStore store) : StateActionHandler<Checkout>(handlerServices, store)
     {
         public override async Task Handle(Checkout action, CancellationToken aCancellationToken)
         {
             var currentState = Store.GetState<CartState>();
+            if (action.Callback.HasDelegate)
+            {
+                await action.Callback.InvokeAsync(null);
+            }
             currentState.Products?.Clear();
             await Persist(currentState);
         }
