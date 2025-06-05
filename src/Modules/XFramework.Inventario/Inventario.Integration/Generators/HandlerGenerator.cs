d5ee88005359e553da7b36a866bdd9a716a86c66
Xeon <eraldo.xeon@gmail.com>
2025-06-05 23:40:02 +0800

    Fix folder structure

--- src/Modules/XFramework.Inventario/Inventario.Integration/Generators/HandlerGenerator.cs
+++ src/Modules/XFramework.Inventario/Inventario.Integration/Generators/HandlerGenerator.cs
@@ -1,7 +1,7 @@
 ﻿using Inventario.Domain.Shared.Contracts;
 using XFramework.Integration.Attributes;
 
-namespace Inventario.Integration;
+namespace Inventario.Integration.Generators;
 
 [StreamFlowWrapper("Inventario.Domain.Shared.Contracts",new[] {
    nameof(Service)
